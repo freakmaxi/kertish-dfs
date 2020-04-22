d7f65cd5c1460efd82782a28c5f6e7a31436d1bf
Tuna Celik <tuna@jakpark.com>
2020-04-22 17:06:34 +0200

    Updated / Fixed paths for create release script

--- -build-/executable/create_release.sh
+++ -build-/executable/create_release.sh
@@ -16,44 +16,44 @@ export RELEASE_VERSION="$major.1.$buildNo"
 
 echo ""
 echo "Building FileSystem command-line tool (v$RELEASE_VERSION)"
-cd ../fs-tool
+cd ../../fs-tool
 echo "  > compiling linux x64 release"
-GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/releases/linux/kertish-dfs
+GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/executable/releases/linux/kertish-dfs
 echo "  > compiling macosx x64 release"
-GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/releases/macosx/kertish-dfs
+GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/executable/releases/macosx/kertish-dfs
 echo "  > compiling windows x64 release"
-GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/releases/windows/kertish-dfs.exe
+GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/executable/releases/windows/kertish-dfs.exe
 
 echo ""
 echo "Building Administrative command-line tool (v$RELEASE_VERSION)"
 cd ../admin-tool
 echo "  > compiling linux x64 release"
-GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/releases/linux/kertish-admin
+GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/executable/releases/linux/kertish-admin
 echo "  > compiling macosx x64 release"
-GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/releases/macosx/kertish-admin
+GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/executable/releases/macosx/kertish-admin
 echo "  > compiling windows x64 release"
-GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/releases/windows/kertish-admin.exe
+GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/executable/releases/windows/kertish-admin.exe
 
 echo ""
 echo "Building Manager Node service executable (v$RELEASE_VERSION)"
-cd ../manager-node/src
+cd ../manager-node
 echo "  > compiling linux x64 release"
-GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../../-build-/releases/linux/kertish-manager
+GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/executable/releases/linux/kertish-manager
 echo "  > compiling macosx x64 release"
-GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../../-build-/releases/macosx/kertish-manager
+GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/executable/releases/macosx/kertish-manager
 
 echo ""
 echo "Building Head Node service executable (v$RELEASE_VERSION)"
-cd ../../head-node/src
+cd ../head-node
 echo "  > compiling linux x64 release"
-GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../../-build-/releases/linux/kertish-head
+GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/executable/releases/linux/kertish-head
 echo "  > compiling macosx x64 release"
-GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../../-build-/releases/macosx/kertish-head
+GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/executable/releases/macosx/kertish-head
 
 echo ""
 echo "Building Data Node service executable (v$RELEASE_VERSION)"
-cd ../../data-node/src
+cd ../data-node
 echo "  > compiling linux x64 release"
-GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../../-build-/releases/linux/kertish-data
+GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/executable/releases/linux/kertish-data
 echo "  > compiling macosx x64 release"
-GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../../-build-/releases/macosx/kertish-data
+GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/executable/releases/macosx/kertish-data
