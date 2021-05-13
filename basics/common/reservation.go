bde18aac112e7f60caeb6436986ec1eff04ce9d3
Tuna Celik <tuna@jakpark.com>
2021-05-13 14:52:51 +0200

    Code clean up

--- basics/common/reservation.go
+++
@@ -1,7 +0,0 @@
-package common
-
-// Reservation struct holds the reservation data for the file uploading
-type Reservation struct {
-	Id   string `json:"reservationId"`
-	Size uint64 `json:"size"`
-}
